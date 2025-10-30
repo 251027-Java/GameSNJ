5431450c3ac0120fbf6d0a1496c4adf2a3135c3b
Sidney Gil <sidneygil@Sidneys-MacBook-Air.local>
2025-10-30 11:56:49 -0500

    added variable names

--- src/main/java/Wordl.java
+++ src/main/java/Wordl.java
@@ -1,12 +1,18 @@
+
+
 public class Wordl {
     //given 5 letter blank word
+        // guessedWord, correctWord
     //ask for input
+        // userWord
     //compare to word
+        // use correctWord as array string to compare individual guessed letters to letters in the correctWord
     //check if word was guessed
         //if correct player wins
     //show user wrong, correct, and partially correct letters
     //ask for new answer
         //update number of guesses our of 6
+            //numGuesses
     //if not guessed correctly show user loses
 
 }
