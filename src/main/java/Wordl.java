d38440a3f6e224d57092db1d7c29d0e773d0aa6f
Sidney Gil <sidneygil@Sidneys-MacBook-Air.local>
2025-10-30 11:42:54 -0500

    psuedo code

---
+++ src/main/java/Wordl.java
@@ -0,0 +1,12 @@
+public class Wordl {
+    //given 5 letter blank word
+    //ask for input
+    //compare to word
+    //check if word was guessed
+        //if correct player wins
+    //show user wrong, correct, and partially correct letters
+    //ask for new answer
+        //update number of guesses our of 6
+    //if not guessed correctly show user loses
+
+}
